a3a200dcbb165b994f6bb3deac651aa3897d29e1
Surajkumbhar905 <skum@ciklum.com>
2025-02-18 17:46:12 +0530

    Initial Commit

---
+++ WhetherApp/WhetherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhetherAppApp.swift
+//  WhetherApp
+//
+//  Created by Suraj-KU on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WhetherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
